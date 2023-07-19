a3a942371786667f4b77010f8ddeeb19deac86c9
eric kyania <erickyania0@gmail.com>
2023-07-19 11:01:22 +0300

    made changes to the game,square and board components interms of how data is passed and shown in the ui

--- src/App.js
+++ src/App.js
@@ -2,21 +2,20 @@ import "./App.css";
 import "./components/styles.css";
 import Square from "./components/square";
 
-function Board([xIsNext, squares, onPlay]) {
+function Board({ xIsNext, squares, onPlay }) {
   // const [xIsNext, setXIsNext] = useState(true);
   // const [squares, setSquare] = useState(Array(9).fill(null));
-  
+
   function handleClick(i) {
-    if (squares[i] || calculateWinner(squares)) {
+    if (calculateWinner(squares) || squares[i]) {
       return;
     }
     const nextSquares = squares.slice();
     if (xIsNext) {
-      nextSquares[i] = "x";
+      nextSquares[i] = "X";
     } else {
-      nextSquares[i] = "o";
+      nextSquares[i] = "O";
     }
-
     onPlay(nextSquares);
   }
 
@@ -92,9 +91,10 @@ function calculateWinner(squares) {
     [2, 4, 6],
   ];
   for (let i = 0; i < lines.length; i++) {
-    const { a, b, c } = lines[i];
+    const [a, b, c] = lines[i];
     if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
       return squares[a];
     }
   }
+  return null;
 }
--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import Game from './components/Game';
+import Game from './components/game';
 import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
